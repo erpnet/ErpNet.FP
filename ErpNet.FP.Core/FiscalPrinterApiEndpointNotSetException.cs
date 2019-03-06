5de651fe6b62d88c06eedfa292ad7cb29314d75c
Dilyan Rusev <dilyanrusev@gmail.com>
2019-03-06 18:57:32 +0200

    initial take on tremol printer

---
+++ ErpNet.FP.Core/FiscalPrinterApiEndpointNotSetException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ErpNet.FP.Core
+{
+    [Serializable]
+    public class FiscalPrinterApiEndpointNotSetException : FiscalPrinterException
+    {
+        public FiscalPrinterApiEndpointNotSetException() { }
+        public FiscalPrinterApiEndpointNotSetException(string message) : base(message) { }
+        public FiscalPrinterApiEndpointNotSetException(string message, Exception inner) : base(message, inner) { }
+        protected FiscalPrinterApiEndpointNotSetException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ ErpNet.FP.Core/FiscalPrinterException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErpNet.FP.Core
+{
+    [Serializable]
+    public class FiscalPrinterException : Exception
+    {
+        public FiscalPrinterException() { }
+        public FiscalPrinterException(string message) : base(message) { }
+        public FiscalPrinterException(string message, Exception inner) : base(message, inner) { }
+        protected FiscalPrinterException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
