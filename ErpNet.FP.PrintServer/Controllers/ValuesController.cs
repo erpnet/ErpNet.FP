477b64a74cc83cbf553d0ae8996c462d08faa607
Dilyan Rusev <dilyanrusev@gmail.com>
2019-03-07 15:35:50 +0200

    support returning of XML and create simple API to play with

--- ErpNet.FP.PrintServer/Controllers/ValuesController.cs
+++ ErpNet.FP.PrintServer/Controllers/ValuesController.cs
@@ -10,36 +10,72 @@ namespace ErpNet.FP.PrintServer.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        public class Todo
+        {
+            public Guid? Id;
+            public string Value;
+        }
+
+        private static Dictionary<Guid, Todo> todos = new Dictionary<Guid, Todo>();
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Todo>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return todos.Values;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Todo> Get(Guid id)
         {
-            return "value";
+            if (todos.TryGetValue(id, out var todo))
+            {
+                return todo;
+            }
+            else
+            {
+                return NotFound();
+            }
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<Todo> Post([FromBody] Todo value)
         {
+            value.Id = Guid.NewGuid();
+            todos.Add(value.Id.Value, value);
+            return CreatedAtAction(nameof(Get), value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult Put(Guid id, [FromBody] Todo value)
         {
+            if (id != value.Id) return BadRequest();
+            if (todos.TryGetValue(id, out var existing))
+            {
+                existing.Value = value.Value;
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(Guid id)
         {
+            if (todos.Remove(id))
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
--- ErpNet.FP.PrintServer/Startup.cs
+++ ErpNet.FP.PrintServer/Startup.cs
@@ -25,7 +25,13 @@ namespace ErpNet.FP.PrintServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(
+                options=>
+                {
+                    options.RespectBrowserAcceptHeader = true;
+                })
+                .AddXmlSerializerFormatters()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +47,7 @@ namespace ErpNet.FP.PrintServer
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
