7f9d0199f22c92b678a44a881101afb11bb5a149
Dean Dimitrovski <dekiland@gmail.com>
2019-03-23 23:30:00 +0200

    fixing shadowing

--- ErpNet.FP.Example/ComTransport.cs
+++ ErpNet.FP.Example/ComTransport.cs
@@ -1,20 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
-using System.Collections.Generic;
 using ErpNet.FP.Print.Core;
 
-namespace ErpNet.FP.Example
-{
+namespace ErpNet.FP.Example {
 	/// <summary>
 	/// Serial COM port transport.
 	/// </summary>
 	/// <seealso cref="ErpNet.FP.Print.Core.Transport" />
-	public class ComTransport : Transport
-	{
+	public class ComTransport : Transport {
 		public override string TransportName => "com";
 
-		public override IChannel OpenChannel(string address) => new Channel(address, 115200, 600);
+		public override IChannel OpenChannel (string address) => new Channel (address, 115200, 600);
 
 		/// <summary>
 		/// Returns all serial com port addresses, which can have connected fiscal printers. 
@@ -23,18 +21,15 @@ namespace ErpNet.FP.Example
 		/// <value>
 		/// All available addresses and descriptions.
 		/// </value>
-		public virtual IEnumerable<(string address, string description)> GetAvailableAddresses()
-		{
-			return from address in SerialPort.GetPortNames() select (address, address);
+		public override IEnumerable < (string address, string description) > GetAvailableAddresses () {
+			return from address in SerialPort.GetPortNames () select (address, address);
 		}
 
-		public class Channel : IChannel
-		{
+		public class Channel : IChannel {
 			private SerialPort _serialPort;
 
-			public Channel(string portName, int baudRate, int timeout)
-			{
-				_serialPort = new SerialPort();
+			public Channel (string portName, int baudRate, int timeout) {
+				_serialPort = new SerialPort ();
 
 				// Allow the user to set the appropriate properties.
 				_serialPort.PortName = portName;
@@ -44,23 +39,21 @@ namespace ErpNet.FP.Example
 				_serialPort.ReadTimeout = timeout;
 				_serialPort.WriteTimeout = timeout;
 
-				_serialPort.Open();
+				_serialPort.Open ();
 			}
 
-			~Channel()
-			{
-				_serialPort.Close();
+			~Channel () {
+				_serialPort.Close ();
 			}
 
 			/// <summary>
 			/// Reads data from the com port.
 			/// </summary>
 			/// <returns>The data which was read.</returns>
-			public Byte[] Read()
-			{
+			public Byte[] Read () {
 				var dataSize = _serialPort.BytesToRead;
 				byte[] data = new byte[dataSize];
-				_serialPort.Read(data, 0, dataSize);
+				_serialPort.Read (data, 0, dataSize);
 				return data;
 			}
 
@@ -68,9 +61,8 @@ namespace ErpNet.FP.Example
 			/// Writes the specified data to the com port.
 			/// </summary>
 			/// <param name="data">The data to write.</param>
-			public void Write(Byte[] data)
-			{
-				_serialPort.Write(data, 0, data.Length);
+			public void Write (Byte[] data) {
+				_serialPort.Write (data, 0, data.Length);
 			}
 		}
 
