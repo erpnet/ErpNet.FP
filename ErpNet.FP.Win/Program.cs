142759a4fe7c681e9e77806ef3ecfaa81667ca23
Dean Dimitrovski <deki@rilasoft.com>
2019-05-07 19:03:04 +0300

    Issue #5
    added logging
    
    
    Former-commit-id: 1c39722af7f3de689094555554a41e46b06c0e2d

--- ErpNet.FP.Win/Program.cs
+++ ErpNet.FP.Win/Program.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Net;
+using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace ErpNet.FP.Win
 {
@@ -10,15 +10,29 @@ namespace ErpNet.FP.Win
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var webHost = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                      .AddJsonFile($"appsettings.{env.EnvironmentName}.json",
+                          optional: true, reloadOnChange: true);
+                config.AddEnvironmentVariables();
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+                logging.AddDebug();
+                logging.AddEventSourceLogger();
+            })
+            .UseStartup<Startup>()
+            .Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            webHost.Run();
+        }
 
     }
 }
