da3fe60ef96d0014a9319222a4eb07ae60ea90a9
Dean Dimitrovski <deki@rilasoft.com>
2019-05-09 16:23:49 +0300

    added debug.log

--- ErpNet.FP.Win/Contexts/PrintersControllerContext.cs
+++ ErpNet.FP.Win/Contexts/PrintersControllerContext.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Net;
 using System.Net.Sockets;
