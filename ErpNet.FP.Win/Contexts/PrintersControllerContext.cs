c8a3b69882d9a3faaae7436b116fa58fad057b14
Dean Dimitrovski <deki@rilasoft.com>
2019-05-09 16:17:48 +0300

    Issue #8
    Former-commit-id: 9644ab22dfd4a37d882a8d90c239e86ad3dc1dae

--- ErpNet.FP.Win/Contexts/PrintersControllerContext.cs
+++ ErpNet.FP.Win/Contexts/PrintersControllerContext.cs
@@ -7,7 +7,11 @@ using ErpNet.FP.Core.Provider;
 using ErpNet.FP.Win.Transports;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ErpNet.FP.Win.Contexts
 {
@@ -32,6 +36,30 @@ namespace ErpNet.FP.Win.Contexts
 
         public Dictionary<string, IFiscalPrinter> Printers { get; } = new Dictionary<string, IFiscalPrinter>();
 
+        public static DateTime GetNetworkTime()
+        {
+            const string ntpServer = "pool.ntp.org";
+            var ntpData = new byte[48];
+            ntpData[0] = 0x1B; // LeapIndicator = 0 (no warning), VersionNum = 3 (IPv4 only), Mode = 3 (Client Mode)
+
+            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+            var ipEndPoint = new IPEndPoint(addresses[0], 123);
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.ReceiveTimeout = 3000; // 3 seconds, prevents hanging when there is no connection
+            socket.Connect(ipEndPoint);
+            socket.Send(ntpData);
+            socket.Receive(ntpData);
+            socket.Close();
+
+            ulong intPart = (ulong)ntpData[40] << 24 | (ulong)ntpData[41] << 16 | (ulong)ntpData[42] << 8 | (ulong)ntpData[43];
+            ulong fractPart = (ulong)ntpData[44] << 24 | (ulong)ntpData[45] << 16 | (ulong)ntpData[46] << 8 | (ulong)ntpData[47];
+
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+            var networkDateTime = (new DateTime(1900, 1, 1)).AddMilliseconds((long)milliseconds);
+
+            return networkDateTime;
+        }
+
         public void AddPrinter(IFiscalPrinter printer)
         {
             // We use serial number of local connected fiscal printers as Printer ID
@@ -52,6 +80,15 @@ namespace ErpNet.FP.Win.Contexts
         public PrintersControllerContext(IConfiguration configuration, ILogger logger)
         {
             this.logger = logger;
+
+            try
+            {
+                logger.LogInformation("NTP Time: " + GetNetworkTime().ToLocalTime().ToString("yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture));
+            }
+            catch
+            {
+                logger.LogCritical("NTP Time cannot be read!");
+            }
 
             var autoDetect = configuration.GetValue<bool>("AutoDetect", true);
 
