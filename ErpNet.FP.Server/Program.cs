607b4be08cc9327ce413ccaa6c36d22494e2ac50
Dean Dimitrovski <deki@rilasoft.com>
2019-05-28 14:00:03 +0300

    Added auto self signing certificate for https

--- ErpNet.FP.Server/Program.cs
+++ ErpNet.FP.Server/Program.cs
@@ -7,12 +7,68 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 
 namespace ErpNet.FP.Server
 {
     public class Program
     {
         private static readonly string DebugLogFileName = @"debug.log";
+
+        private static IEnumerable<IPAddress> GetLocalV4Addresses()
+        {
+            return from iface in NetworkInterface.GetAllNetworkInterfaces()
+                   where iface.OperationalStatus == OperationalStatus.Up
+                   from address in iface.GetIPProperties().UnicastAddresses
+                   where address.Address.AddressFamily == AddressFamily.InterNetwork
+                   select address.Address;
+        }
+
+        private static X509Certificate2 BuildSelfSignedServerCertificate()
+        {
+            const string CertificateName = "ErpNet.FP";
+
+            SubjectAlternativeNameBuilder sanBuilder = new SubjectAlternativeNameBuilder();
+
+            sanBuilder.AddIpAddress(IPAddress.Loopback);
+            sanBuilder.AddIpAddress(IPAddress.IPv6Loopback);
+            sanBuilder.AddDnsName(Environment.MachineName);
+            sanBuilder.AddDnsName("localhost");
+
+            var addresses = GetLocalV4Addresses();
+            foreach (var address in addresses)
+            {
+                sanBuilder.AddIpAddress(address);
+            }
+
+            X500DistinguishedName distinguishedName = new X500DistinguishedName($"CN={CertificateName}");
+
+            using (RSA rsa = RSA.Create(2048))
+            {
+                var request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+                request.CertificateExtensions.Add(
+                    new X509KeyUsageExtension(X509KeyUsageFlags.DataEncipherment | X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DigitalSignature, false));
+
+
+                request.CertificateExtensions.Add(
+                   new X509EnhancedKeyUsageExtension(
+                       new OidCollection { new Oid("1.3.6.1.5.5.7.3.1") }, false));
+
+                request.CertificateExtensions.Add(sanBuilder.Build());
+
+                var certificate = request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddDays(3650)));
+                certificate.FriendlyName = CertificateName;
+
+                return new X509Certificate2(certificate.Export(X509ContentType.Pfx, "ErpNet.FP.Password"), "ErpNet.FP.Password", X509KeyStorageFlags.MachineKeySet);
+            }
+        }
 
         public static void Main()
         {
@@ -37,27 +93,31 @@ namespace ErpNet.FP.Server
             Trace.WriteLine("Starting the application...");
 
             var webHost = new WebHostBuilder()
-            .UseKestrel()
-            .UseContentRoot(Directory.GetCurrentDirectory())
-            .ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                var env = hostingContext.HostingEnvironment;
-                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-                config.AddEnvironmentVariables();
-            })
-            .ConfigureKestrel((hostingContext, options) =>
-            {
-                options.Configure(hostingContext.Configuration.GetSection("Kestrel"));
-            })
-            .ConfigureLogging((hostingContext, logging) =>
-            {
-                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                logging.AddConsole();
-                logging.AddDebug();
-                logging.AddEventSourceLogger();
-            })
-            .UseStartup<Startup>()
-            .Build();
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureKestrel((hostingContext, options) =>
+                {
+                    options.Configure(hostingContext.Configuration.GetSection("Kestrel"));
+                    options.ConfigureHttpsDefaults(httpsOptions =>
+                    {
+                        httpsOptions.ServerCertificate = BuildSelfSignedServerCertificate();
+                    });
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                })
+                .UseStartup<Startup>()
+                .Build();
 
             var logger = webHost.Services.GetRequiredService<ILogger<Program>>();
             logger.LogInformation("Starting the service...");
